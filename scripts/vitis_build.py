7e312a117de4ebbac836fe2c5032a07e96f61c10
Josue Ortiz <ortizjo137@gmail.com>
2025-04-20 08:07:06 -0400

    Modified the Vitis build flow to check if components already exists #1
     - This allows the script to be run for cleaning/building or creation
     - Vitis raises exceptions when trying to create components that already
       exists
     - Did the platform, domain, and domain bsp_libs part. Need to do the
       app parts

--- scripts/vitis_build.py
+++ scripts/vitis_build.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import json
 import vitis # AMD Vitis CLI package
 
 # sert path and name vars from command-line input
@@ -12,27 +13,62 @@ sys.path.append(sw_path) # config.py is in the sw_path
 
 import config
 
+def get_platform_file(vitis_client, platform_dir):
+    platform_xpfm=vitis_client.find_platform_in_repos(platform_dir)
+    return platform_xpfm
+
+def get_component(vitis_client, comp_name):
+    try: comp = vitis_client.get_component(comp_name)
+    except: return None
+    else : return comp
+
+def get_domain(platform, dom_name):
+    try: dom = platform.get_domain(dom_name)
+    except: return None
+    else : return dom
+
+def lib_in_bsp(domain, lib_name):
+    dom_libs = domain.get_libs()
+    lib_names = []
+    for dicts in dom_libs:
+        lib_names.append(dicts["name"])
+    return (lib_name in lib_names)
 
 client = vitis.create_client()
 client.set_workspace(path="./")
 
-# first step is to create the platform with details
-# from the config.py file
-platform = client.create_platform_component(name = config.platform_name,
-                                            hw_design = xsa_file,
-                                            os = config.os_name,
-                                            cpu = config.cpu_name, 
-                                            no_boot_bsp = False)
+# the platform is the first component that needs to be created
+platform = get_component(client, config.platform_name)
+if not platform:
+    print("Platform " + config.platform_name + " does not exist")
+    print("Creating the platform")
+    # first step is to create the platform with details
+    # from the config.py file
+    platform = client.create_platform_component(name = config.platform_name,
+                                                hw_design = xsa_file,
+                                                os = config.os_name,
+                                                cpu = config.cpu_name, 
+                                                no_boot_bsp = False)
 
-# default domain created is name by concatenating os_name and _cpu_name
+# ensure the domain exists
 domain_name = config.os_name + "_"+ config.cpu_name
-# before building, add any libraries caleld out in config.py
+domain = get_domain(platform, domain_name)
+if not domain:
+    print("Domain " + domain_name + " does not exist")
+    print("Creating the domain")
+    domain = platform.add_domain(name = domain_name, 
+                                 cpu = config.cpu_name, 
+                                 os = config.os)
+    
+# ensure the bsp libraries have been added
 if config.bsp_libs != None:
-    platform = client.get_component(config.platform_name)
-    domain = platform.get_domain(domain_name)
+    print("User bsp_entries not empty")
+    print("Checking if bsp_libs are already added")
     for lib_name in config.bsp_libs:
-        status = domain.set_lib(lib_name=lib_name)
-status = platform.build()
+        if not lib_in_bsp(domain, lib_name):
+            print("Adding " + lib_name +" lib")
+            domain.set_lib(lib_name=lib_name)
+platform.build()
 
 # Second step is to create the application that will run on the platform
 # If a app template is provided in config.py, check that it's valid
