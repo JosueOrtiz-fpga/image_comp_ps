20347c07f9b072c0b8ef61dd3e8849b11cdc46bc
Josue Ortiz <ortizjo137@gmail.com>
2025-05-11 05:21:38 -0400

    Successful image division and concatenation #2

--- models/dct.py
+++ models/dct.py
@@ -16,50 +16,50 @@ def calc_2d_dct(input_matrix):
     dct2_t_mat_transpose = dct2_mat(8).T
     return np.dot(dct2_t_mat_transpose, np.dot(dct2_t_mat, input_matrix))
 
+def img_2_blocks(img, n):
+    height, width, depth = img.shape
+    matrices = []
+    for i in range(0, (height+1) - n, n):
+        for j in range(0, (width+1) - n, n):
+            matrix = img[i:i + n, j:j + n]
+            matrices.append(matrix)
+    return matrices
+
+def blocks_2_img(blocks, height, width):
+    # cocnatenating
+    num_blocks_row = int((width + 1)/8)
+    num_rows = int((height + 1)/8)
+    img2_rows_of_blocks = []
+
+    # concatenate blocks into rows
+    for i in range(num_rows):
+        img2_rows_of_blocks.append(np.concatenate(blocks[num_blocks_row*i:num_blocks_row*(i+1)],axis=1))
+
+    #concatenate rows into matrix
+    img2 = np.concatenate(img2_rows_of_blocks, axis=0)
+    return img2
+
+def mask(n, coef_num, mat):
+    # generate mask
+    height, width, depth = mat.shape
+    mask_matrix = np.zeros((height,width,depth),'unit8')
+    for m in range(0,n):
+        for n in range(0,n-m):
+            mask_matrix[m,n] = (1,1,1)
+    # apply mask
+    return mat * mask_matrix
+
 # image reading
 img = cv2.imread("cameraman.tiff")
 height, width, depth = img.shape
 
-# image division into 8x8 blocks
-matrices = []
-for i in range(0, (height+1) - 8, 8):
-    for j in range(0, (width+1) - 8, 8):
-        matrix = img[i:i + 8, j:j + 8]
-        matrices.append(matrix)
+matrices = img_2_blocks(img, 8)
+img2 = blocks_2_img(matrices, height, width)
 
-# dct of all 8x8 blocks
-dct_blocks = []
-for block in matrices:
-    dct_blocks.append(calc_2d_dct(block))
-
-# masking
-mask_matrix = np.zeros((8,8,3),img.dtype)
-for m in range(0,4):
-    for n in range(0,4-m):
-        mask_matrix[m,n] = (1,1,1)
-
-dct_blocks_masked = []
-for block in dct_blocks:
-    dct_blocks_masked.append(block * mask_matrix)
-
-# inverse dct
-img2_blocks = []
-for block in dct_blocks_masked:
-    img2_blocks.append(calc_2d_dct(block))
-# cocnatenating
-num_blocks_row = int((width + 1)/8)
-num_rows = int((height + 1)/8)
-img2_rows_of_blocks = []
-
-# concatenate blocks into rows
-for i in range(num_rows):
-    img2_rows_of_blocks.append(np.concatenate(img2_blocks[0:num_blocks_row],axis=1))
-
-#concatenate rows into matrix
-img2 = np.concatenate(img2_rows_of_blocks, axis=0)
 print(img2.shape)
 
 # display
-cv2.imshow("Image", img2)
+cv2.imshow("Image 1", img)
+cv2.imshow("Image 2", img2)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
