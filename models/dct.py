b55e6466f6c40962f6b437612fb9bdaf90fa3e0f
Josue Ortiz <ortizjo137@gmail.com>
2025-05-12 06:36:37 -0400

    Added Python docstrings and refactored code #2

--- models/dct.py
+++ models/dct.py
@@ -2,6 +2,14 @@ import numpy as np
 import cv2
 
 def dct2_mat(n):
+    """
+    Returns an n x n DCT tranform matrix
+
+    Parameters:
+        n(int): matrix dimension desired
+    Returns:
+        mat(numpy.ndarray): An n x n DCT transform matrix 
+    """
     mat = np.zeros((n,n))
     for p in range(n):
         for q in range(n):
@@ -11,91 +19,133 @@ def dct2_mat(n):
                 mat[p,q] = np.sqrt(2/n) * np.cos((np.pi*(2*q+1)*p)/(2*n))
     return mat
 
-def calc_2d_dct(input_matrix):
-    dct2_t_mat = dct2_mat(8)
-    dct2_t_mat_transpose = dct2_t_mat.T
-    return dct2_t_mat @ input_matrix @ dct2_t_mat_transpose
+def calc_2d_dct(matrix):
+    """
+    Returns the 2-D DCT of the input matrix
 
-def calc_2d_idct(input_matrix):
-    dct2_t_mat = dct2_mat(8)
+    Parameters:
+        matrix(numpy.ndarray): matrix, n x n
+    Returns:
+        dct2(numpy.ndarray): 2D DCT transform of the input matrix 
+    """
+    dct2_t_mat = dct2_mat(matrix.shape[0])
     dct2_t_mat_transpose = dct2_t_mat.T
-    return dct2_t_mat_transpose @ input_matrix @ dct2_t_mat
+    return dct2_t_mat @ matrix @ dct2_t_mat_transpose
+
+def calc_2d_idct(matrix):
+    """
+    Returns the 2-D Inverse DCT of the input matrix
+
+    Parameters:
+        matrix(numpy.ndarray): matrix of 2-D DCT coefficients, n x n
+    Returns:
+        idct2(numpy.ndarray): Inverse DCT, n x n matrix
+    """
+    dct2_t_mat = dct2_mat(matrix.shape[0])
+    dct2_t_mat_transpose = dct2_t_mat.T
+    return dct2_t_mat_transpose @ matrix @ dct2_t_mat
 
 def img_2_blocks(img, n):
-    shape = img.shape
-    if len(shape) == 3:
-        height, width, depth = img.shape
-    if len(shape) == 2:
-        height,width = img.shape
+    """
+    Breaks the input image into n x n blocks
+
+    Parameters:
+        img(numpy.ndarray): matrix representing an image
+        n(int)            : block size to be used
+    Returns:
+        matrices(list): list of n x n blocks
+    """
     matrices = []
-    for i in range(0, (height+1) - n, n):
-        for j in range(0, (width+1) - n, n):
+    for i in range(0, (img.shape[0]+1) - n, n):
+        for j in range(0, (img.shape[1]+1) - n, n):
             matrix = img[i:i + n, j:j + n]
             matrices.append(matrix)
     return matrices
 
 def blocks_2_img(blocks, height, width):
-    # cocnatenating
-    num_blocks_row = int((width + 1)/8)
-    num_rows = int((height + 1)/8)
-    img2_rows_of_blocks = []
+    """
+    Concatenates input blocks into an image (matrix)
 
-    # concatenate blocks into rows
+    Parameters:
+        blocks(list) : list of n x n blocks to concatenate
+        height(int)  : expected height of return image
+        width(int)   : expected width of return image
+    Returns:
+        img(np.ndarray): matrix of concatenated blocks
+    """
+
+    n = blocks[0].shape[0]
+    blocks_p_row = int((width + 1)/n)
+    num_rows = int((height + 1)/n)
+
+    rows = []
     for i in range(num_rows):
-        img2_rows_of_blocks.append(np.concatenate(blocks[num_blocks_row*i:num_blocks_row*(i+1)],axis=1))
+        rows.append(np.concatenate(blocks[blocks_p_row*i:blocks_p_row*(i+1)],axis=1))
 
-    #concatenate rows into matrix
-    img2 = np.concatenate(img2_rows_of_blocks, axis=0)
-    return img2
+    img = np.concatenate(rows, axis=0)
+    return img
 
 def dct_blocks(blocks):
+    """
+    Applies the 2D DCT to a list of blocks
+
+    Parameters:
+        blocks(list) : list of n x n blocks
+    Returns:
+        blocks_tf(list): list of n x n transformed blocks
+    """
     blocks_tf = []
     for block in blocks:
         blocks_tf.append(calc_2d_dct(block))
     return blocks_tf
 
 def idct_blocks(blocks):
+    """
+    Applies the 2D Inverse DCT to a list of blocks
+
+    Parameters:
+        blocks(list) : list of n x n blocks with DCT corefficients
+    Returns:
+        blocks_tf(list): list of n x n transformed blocks
+    """
     blocks_tf = []
     for block in blocks:
         blocks_tf.append(calc_2d_idct(block))
     return blocks_tf
 
-def float_2_int(input_matrix):
-    return input_matrix.astype(np.uint8)
-
 def int_blocks(blocks):
+    """
+    Converts input blocks' members to np.unit8 type
+
+    Parameters:
+        blocks(list) : list of n x n blocks with DCT corefficients
+    Returns:
+        blocks_int(list): list of n x n transformed blocks
+    """
     blocks_int = []
     for block in blocks:
-        blocks_int.append(float_2_int(block))
+        blocks_int.append(block.astype(np.uint8))
     return blocks_int
 
-def mask(n, coef_num, mat):
-    # generate mask
-    height, width, depth = mat.shape
-    mask_matrix = np.zeros((height,width,depth),'unit8')
-    for m in range(0,n):
-        for n in range(0,n-m):
-            mask_matrix[m,n] = (1,1,1)
-    # apply mask
-    return mat * mask_matrix
+# def mask(n, coef_num, mat):
+#     # generate mask
+#     height, width, depth = mat.shape
+#     mask_matrix = np.zeros((height,width,depth),'unit8')
+#     for m in range(0,n):
+#         for n in range(0,n-m):
+#             mask_matrix[m,n] = (1,1,1)
+#     # apply mask
+#     return mat * mask_matrix
 
 # image reading
-img= cv2.imread("cameraman.tiff",cv2.IMREAD_GRAYSCALE)
-height, width = img.shape
+src_img= cv2.imread("cameraman.tiff",cv2.IMREAD_GRAYSCALE)
+blocks = int_blocks(idct_blocks(dct_blocks(img_2_blocks(src_img, 8))))
 
-matrices = img_2_blocks(img, 8)
-matrices2 = int_blocks(idct_blocks(dct_blocks(matrices)))
-img1 = blocks_2_img(matrices,height,width)
-img2 = blocks_2_img(matrices2,height, width)
-
-print(img1)
-print('\n')
-print('\n')
-print('\n')
-print(img2)
+height, width = src_img.shape
+result_img = blocks_2_img(blocks,height, width)
 
 #display
-cv2.imshow("Image 1", img1)
-cv2.imshow("Image 2", img2)
+cv2.imshow("Image 1", src_img)
+cv2.imshow("Image 2", result_img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
