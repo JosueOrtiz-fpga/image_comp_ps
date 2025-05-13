f43fd8e45bc1b2b79138def46352298c7988b958
Josue Ortiz <ortizjo137@gmail.com>
2025-05-12 21:11:45 -0400

    Added functions to convert a whole matrix from one color space to another #4

--- models/color.py
+++ models/color.py
@@ -1,4 +1,5 @@
 import numpy as np
+
 def rgb_2_ycbcr(rgb):
     """
     Converts an RGB pixel to a the YCbCr color space
@@ -28,3 +29,42 @@ def ycbcr_2_rgb(ycbcr):
     g = y - 0.344136*(cb-128) - 0.714136*(cr-128)
     b = y + 1.772*(cb-128)
     return (r,g,b)
+
+def rgb_mat_2_ycbcr(mat):
+    """
+    Converts a matrix of RGB pixels to the YCbCr color space
+
+    Parameters:
+        mat(ndarray) : a matrix of RGB pixels
+    Returns:
+        ycbcr_mat(ndarray): a matrix of YCbCr pixels
+    """
+    height, width, depth = mat.shape
+    ycbcr_mat = np.zeros((height, width, depth), dtype=mat.dtype)
+    for m in range(height):
+        for n in range(width):
+            ycbcr_mat[m][n] = np.uint8(rgb_2_ycbcr(mat[m][n]))
+    return ycbcr_mat
+
+def ycbcr_mat_2_rgb(mat):
+    """
+    Converts a matrix of YCbCr pixels to the RGB color space
+
+    Parameters:
+        mat(ndarray) : a matrix of YCbCr pixels
+    Returns:
+        rgb_mat(ndarray): a matrix of RGB pixels
+    """
+    height, width, depth = mat.shape
+    rgb_mat = np.zeros((height, width, depth), dtype=mat.dtype)
+    for m in range(height):
+        for n in range(width):
+            rgb_mat[m][n] = np.uint8(ycbcr_2_rgb(mat[m][n]))
+    return rgb_mat
+
+test_mat = np.random.random_integers(50,255,(1,8,3))
+print(test_mat)
+print(rgb_mat_2_ycbcr(test_mat))
+print(ycbcr_mat_2_rgb(rgb_mat_2_ycbcr(test_mat)))
+
+
