0529b966911339dd1b461626a5d91860c806b417
Josue Ortiz <ortizjo137@gmail.com>
2025-05-12 20:41:57 -0400

    Added pixel conversion from ycbcr to rgb

--- models/color.py
+++ models/color.py
@@ -1,4 +1,4 @@
-
+import numpy as np
 def rgb_2_ycbcr(rgb):
     """
     Converts an RGB pixel to a the YCbCr color space
@@ -9,8 +9,22 @@ def rgb_2_ycbcr(rgb):
         pixel(tupple): a YCbCr pixel
     """
     red, green, blue = rgb
-    pixel = (red,green,blue)
-    return pixel
+    y= 0 + (0.299*red) + (0.587 * green) + (0.114 * blue)
+    cb = 128 - (0.168736 * red) - (0.331264 * green) + (0.5 * blue)
+    cr = 128 + (0.5 * red) - (0.418688 * green) - (0.081312 * blue)
+    return (y,cb,cr)
 
-print(rgb_2_ycbcr((10,11,12)))
-print(type(rgb_2_ycbcr((10,11,12))))
+def ycbcr_2_rgb(ycbcr):
+    """
+    Converts an YCbCr pixel to a the RGB color space
+
+    Parameters:
+        ycbcr(tupple) : a Y-Cb-Cr pixel
+    Returns:
+        pixel(tupple): a RGB pixel
+    """
+    y,cb,cr = ycbcr
+    r = y + 1.402*(cr-128)
+    g = y - 0.344136*(cb-128) - 0.714136*(cr-128)
+    b = y + 1.772*(cb-128)
+    return (r,g,b)
