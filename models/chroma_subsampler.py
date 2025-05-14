5a8ab898ee8b10cd6f7c3c7b3c5a74e4357dd463
Josue Ortiz <ortizjo137@gmail.com>
2025-05-14 06:48:34 -0400

    Previous commit only did splitting matrix into rows of pixel #6
     - Here we comprise a resulting matrix based on rows
     - Couldn't get np.concatenat to work: did it using for loops, need to
       debug

--- models/chroma_subsampler.py
+++ models/chroma_subsampler.py
@@ -17,19 +17,29 @@ def chroma_subsample_mat(mat):
         cb = []
         cr=[]
         for pixel in row:
-            y.append(pixel[0])
-            cb.append(pixel[1])
-            cr.append(pixel[2])
+            y.append(np.uint8(pixel[0]))
+            cb.append(np.uint8(pixel[1]))
+            cr.append(np.uint8(pixel[2]))
         y_row.append(y)
-        cb_row.append(cb)
-        cr_row.append(cr)
+        
+        # cb_row.append(np.ndarray(cb))
+        # cr_row.append(np.ndarray(cr))
+    # print("-------------")
+    height, width, depth = mat.shape
+    y_mat = np.zeros((height, width),mat.dtype)
+    for m in range(len(y_row)):
+        y_mat[m] = y_row[m]
     print(mat)
-    print("----------------")
-    print(y_row)
-    print(cb_row)
-    print(cr_row)
-        
+    print("-------------")
+    print(y_mat)
+    # y_mat = np.concatenate(y_row, axis=1)
+    # print(y_mat)
+    # cb_mat = np.concatenate(cb_row, axis=0)
+    # cr_mat = np.concatenate(cr_row, axis=0)
+    # print(mat)
+    # print("-------------")
+    # print(y_mat)
         
 
 
-chroma_subsample_mat(np.random.random_integers(50,255,(2,4,3)))
+chroma_subsample_mat(np.random.random_integers(50,255,(3,4,3)))
