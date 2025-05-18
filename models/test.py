3bf3713aa7aa544c3e2d9feb9d61ea14db7ce41c
Josue Ortiz <ortizjo137@gmail.com>
2025-05-18 07:05:29 -0400

    block_subsample and block_subsample_avg test pass #11

--- models/test.py
+++ models/test.py
@@ -17,6 +17,8 @@ def test_img_color_conv():
     """ Image Color Conversion Test"""
     test_img = np.random.randint(0,255,(256,256,3))
     result = color.ycbcr_mat_2_rgb(color.rgb_mat_2_ycbcr(test_img))
+
+    # checkign each element in the arrays
     for t_row,r_row in zip(test_img,result):
         for t_pixel, r_pixel in zip(t_row, r_row):
             for t, r in zip(t_pixel,r_pixel):
@@ -29,6 +31,56 @@ def test_img_color_conv():
 """
 Subsampler Tests
 """
+import chroma_subsampler as cs
+def test_block_sub_sample():
+    """
+    Block SubSample Test
+    """
+    num_bloks = 3
+    test_blocks  = []
+    for i in range(num_bloks):
+        test_blocks.append(np.random.randint(0,255,(2,4)))
+    
+    results = []
+    for block in test_blocks:
+        results.append(cs.block_subsample(block))
+    
+    for block in test_blocks:
+        block[:,0:2].fill(block[0,0])
+        block[:,2:4].fill(block[0,2])
 
-# test_pixel_color_conv()
-# test_img_color_conv()
+    # checkign each element in the arrays
+    for test_block, result_block in zip(test_blocks, results):
+        for t_row, r_row in zip(test_block,result_block):
+            for t_n, r_n in zip(t_row, r_row):
+                assert(t_n == r_n)
+    print("block_sub_sample PASS")    
+
+def test_block_sub_sample_avg():
+    """
+    Block SubSample Test
+    """
+    num_bloks = 3
+    test_blocks  = []
+    for i in range(num_bloks):
+        test_blocks.append(np.random.randint(0,255,(2,4)))
+    
+    results = []
+    for block in test_blocks:
+        results.append(cs.block_subsample_avg(block))
+    
+    for block in test_blocks:
+        block[:,0:2].fill(np.mean(block[:,0:2]))
+        block[:,2:4].fill(np.mean(block[:,2:4]))
+    
+    # checkign each element in the arrays
+    for test_block, result_block in zip(test_blocks, results):
+        for t_row, r_row in zip(test_block,result_block):
+            for t_n, r_n in zip(t_row, r_row):
+                assert(t_n == r_n)
+    print("block_sub_sample_avg PASS")   
+
+test_pixel_color_conv()
+test_img_color_conv()
+test_block_sub_sample()
+test_block_sub_sample_avg()
