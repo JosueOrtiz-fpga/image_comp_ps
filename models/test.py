d6d096d5cb4d01db03ad403bceaf63baec90e92f
Josue Ortiz <ortizjo137@gmail.com>
2025-06-12 21:32:08 -0400

    Checked out the split_img method

--- models/test.py
+++ models/test.py
@@ -24,11 +24,19 @@ class TestColorConvMethod(unittest.TestCase):
         self.assertTrue(np.array_equal(img_exp,img_res))
     
     def test_rand(self):
-        rand_img = np.random.randint(0,256,(4,4,3), np.uint8)
+        rand_img = np.random.randint(0,256,(1024,512,3), np.uint8)
         img_exp = self.ycrcb_2_ycbcr(cv2.cvtColor(rand_img, cv2.COLOR_RGB2YCrCb))
         img_res = JPEGEncoder.rgb_2_ycbcr(rand_img)
         # convert to int16 to avoid underflow due to a -1 result from subtaction
         self.assertTrue(np.all(np.abs(img_exp.astype(np.int16) - img_res.astype(np.int16)) <= 1))
 
+class TestSplitImageMethod(unittest.TestCase):
+    def test_split_img(self):
+         rand_img = np.random.randint(0,256,(1024,512,3), np.uint8)
+         y_res, cb_res, cr_res = JPEGEncoder.split_img(rand_img)
+         self.assertTrue(np.array_equal(rand_img[:,:,0],y_res))
+         self.assertTrue(np.array_equal(rand_img[:,:,1],cb_res))
+         self.assertTrue(np.array_equal(rand_img[:,:,2],cr_res))
+    
 if __name__ == '__main__':
     unittest.main()
